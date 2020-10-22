f3ef052d3cc5ba7251adc254f4e7d69336b4074d
Shrutam98 <shrutam98@tatvasoft.com>
2020-10-22 12:24:26 +0530

    new

--- shoppingcart_redux/src/App.js
+++ shoppingcart_redux/src/App.js
@@ -5,8 +5,6 @@ import { Input } from "antd";
 import "antd/dist/antd.css";
 import { Provider } from "react-redux";
 import { store } from "actions/store";
-import CategoryList from "components/Category/CategoryList";
-import ProductList from "components/Product/ProductList";
 import Navingation from "components/Shared/Navingation";
 
 function App() {
